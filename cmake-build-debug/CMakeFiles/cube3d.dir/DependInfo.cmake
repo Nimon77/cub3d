edf0cedef9206971a19dc7e335756b2c59f08db7
nsimon <nsimon@student.42.fr>
2020-02-19 19:30:49 +0100

    cub3d

---
+++ cmake-build-debug/CMakeFiles/cube3d.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_atoi.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_atoi.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_bzero.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_bzero.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_calloc.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_calloc.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_isalnum.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_isalnum.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_isalpha.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_isalpha.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_isascii.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_isascii.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_isdigit.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_isdigit.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_isprint.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_isprint.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_itoa.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_itoa.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstadd_back.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstadd_back.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstadd_front.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstadd_front.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstclear.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstclear.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstdelone.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstdelone.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstiter.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstiter.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstlast.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstlast.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstmap.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstmap.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstnew.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstnew.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_lstsize.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_lstsize.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_memccpy.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_memccpy.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_memchr.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_memchr.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_memcmp.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_memcmp.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_memcpy.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_memcpy.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_memmove.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_memmove.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_memset.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_memset.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_putchar_fd.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_putchar_fd.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_putendl_fd.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_putendl_fd.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_putnbr_fd.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_putnbr_fd.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_putstr_fd.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_putstr_fd.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_split.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_split.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strchr.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strchr.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strdup.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strdup.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strjoin.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strjoin.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strlcat.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strlcat.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strlcpy.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strlcpy.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strlen.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strlen.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strmapi.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strmapi.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strncmp.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strncmp.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strnstr.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strnstr.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strrchr.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strrchr.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_strtrim.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_strtrim.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_substr.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_substr.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_tolower.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_tolower.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/libft/ft_toupper.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/libft/ft_toupper.c.o"
+  "/Users/nsimon/CLionProjects/cub3d/srcs/main.c" "/Users/nsimon/CLionProjects/cub3d/cmake-build-debug/CMakeFiles/cube3d.dir/srcs/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../includes"
+  "../minilibx"
+  "../libft"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ libft/ft_memset.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsimon <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 11:42:35 by nsimon            #+#    #+#             */
+/*   Updated: 2020/01/29 17:44:02 by nsimon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)b;
+	while (i < len)
+		ptr[i++] = (unsigned char)c;
+	return (b);
+}
