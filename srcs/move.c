4c31315d3aa3bbae3a9d3ed517164fd892b909fd
nsimon <nsimon@student.42.fr>
2020-05-21 21:15:11 +0200

    Correction movements

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: nsimon <nsimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 20:37:44 by nsimon            #+#    #+#             */
-/*   Updated: 2020/05/18 16:55:09 by nsimon           ###   ########.fr       */
+/*   Updated: 2020/05/21 18:57:15 by nsimon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,33 @@
 
 void	go_up(t_cub *cub)
 {
-	int moveSpeed = 5;
+	float moveSpeed = 0.2;
 	
-	if (cub->map[(int)(cub->plane.x + cub->dir.x * moveSpeed)][(int)
-			cub->plane.y] == '0')
-		cub->plane.x += cub->dir.x * moveSpeed;
-	if (cub->map[(int)cub->plane.x][(int)(cub->plane.y + cub->dir.y *
-			moveSpeed)] == '0')
-		cub->plane.y += cub->dir.y * moveSpeed;
+	if (cub->map[(int)(cub->pos.x + cub->dir.x * (moveSpeed + 0.1))]
+			[(int)cub->pos.y] == '0')
+		cub->pos.x += cub->dir.x * moveSpeed;
+	if (cub->map[(int)cub->pos.x]
+			[(int)(cub->pos.y + cub->dir.y * (moveSpeed + 0.1))] == '0')
+		cub->pos.y += cub->dir.y * moveSpeed;
 }
 
 void	go_down(t_cub *cub)
 {
-	int moveSpeed = 5;
+	float moveSpeed = 0.2;
 	
-	if (cub->map[(int)(cub->plane.x + cub->dir.x * moveSpeed)][(int)
-			cub->plane.y] == '0')
-		cub->plane.x -= cub->dir.x * moveSpeed;
-	if (cub->map[(int)cub->plane.x][(int)(cub->plane.y + cub->dir.y *
-			moveSpeed)] == '0')
-		cub->plane.y -= cub->dir.y * moveSpeed;
+	if (cub->map[(int)(cub->pos.x - cub->dir.x * (moveSpeed + 0.1))]
+		[(int)cub->pos.y] == '0')
+		cub->pos.x -= cub->dir.x * moveSpeed;
+	if (cub->map[(int)cub->pos.x]
+		[(int)(cub->pos.y - cub->dir.y * (moveSpeed + 0.1))] == '0')
+		cub->pos.y -= cub->dir.y * moveSpeed;
 }
 
 void	go_left(t_cub *cub)
 {
 	double old_d_x = cub->dir.x;
 	double old_p_x = cub->plane.x;
-	int rotSpeed = 1;
+	float rotSpeed = 0.1;
 	
 	cub->dir.x = cub->dir.x * cos(rotSpeed) - cub->dir.y * sin(rotSpeed);
 	cub->dir.y = old_d_x * sin(rotSpeed) + cub->dir.y * cos(rotSpeed);
@@ -53,7 +53,7 @@ void	go_right(t_cub *cub)
 {
 	double	old_d_x = cub->dir.x;
 	double	old_p_x = cub->plane.x;
-	int rotSpeed = 1;
+	float rotSpeed = 0.1;
 	
 	cub->dir.x = cub->dir.x * cos(-rotSpeed) - cub->dir.y * sin(-rotSpeed);
 	cub->dir.y = old_d_x * sin(-rotSpeed) + cub->dir.y * cos(-rotSpeed);
