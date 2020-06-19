1bc0b70ff41518be33b5c8bb3f230547fe5c8fcf
nsimon <nsimon@student.42.fr>
2020-06-19 15:03:54 +0200

    test_error valgrind

--- test_error.sh
+++ test_error.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 make
-find error_test -type f -exec echo -e "\ntest {}" \; -exec ./Cub3D {} \;
+#find error_test -type f -exec echo -e "\ntest {}" \; -exec ./cub3D {} \;
+find error_test -type f -exec echo -e "\ntest {}" \; -exec valgrind --undef-value-errors=no ./cub3D {} \; -exec echo "" \;
 
